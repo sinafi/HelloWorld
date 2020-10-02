4e5e3b2baae8ab2ec546648434c4159b6226eb0c
Sina Fi <34649157+sinafi@users.noreply.github.com>
2020-10-02 06:40:20 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+  cout<<"Welcome To Hello World V2.0|Developed By:Sina Fi|https://github.com/sinafi/HelloWorld"<<endl<<"Enter Your Language"<<endl<<"1.English 2.Persian 3.Turkish 4.French"<<endl;
+  int lang;
+  cin>>lang;
+  if(lang==1)
+  {
+    cout<<"Hello World";
+  }
+  if(lang==2)
+  {
+    cout<<"Salam Donya";
+  }
+  if(lang==3)
+  {
+    cout<<"Selam Dünya";
+  }
+  if(lang==4)
+  {
+    cout<<"Bonjour le monde";
+  }
+  else()
+  {
+  cout<<"Restart Console Application"
+  }
+  return 0;
+}
